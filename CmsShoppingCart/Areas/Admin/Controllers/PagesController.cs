48270fabf56bd6ddd50ce27edda74ce1cecbf1fc
Norbert Leśniak <norbert.lesniak@gmail.com>
2018-05-24 12:19:24 +0200

    Add Delete > Pages

--- CmsShoppingCart/Areas/Admin/Controllers/PagesController.cs
+++ CmsShoppingCart/Areas/Admin/Controllers/PagesController.cs
@@ -204,5 +204,26 @@ namespace CmsShoppingCart.Areas.Admin.Controllers
                 return View(model);
         }
 
+        // GET: Admin/Pages/DeletePage/id
+        public ActionResult DeletePage(int id)
+        {
+            using (Db db = new Db())
+            {
+                // Get the page
+                PageDTO dto = db.Pages.Find(id);
+
+                // Remove the page
+                db.Pages.Remove(dto);
+
+                // Save
+                db.SaveChanges();
+
+            }
+
+            // Redirect
+            return RedirectToAction("Index");
+
+        }
+
     }
 }
